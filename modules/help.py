626fd7cdd7d1d85d64a3eaffc89aee8bf0f106af
ThalusA <alexandre.monier@epitech.eu>
2019-05-27 10:31:32 +0200

    Fixing help by assigning a new var that point to the module

--- modules/help.py
+++ modules/help.py
@@ -15,7 +15,8 @@ class help:
 				permissions = f" ( Required Permission : {getattr(self.Modules[args], args).permission} )"
 			helpOutput += f"{self.Modules[args].__name__}: {getattr(self.Modules[args], args).description}{permissions}"
 		else : 
-			for mod in self.Modules.values():
+			for module_name in self.Modules:
+				mod = self.Modules[module_name]
 				if (mod.__name__ != "baseModule" and getattr(mod, mod.__name__).shownInHelp == True):
 					if (getattr(mod, mod.__name__).permission):
 						permissions = f' ( Required Permission: {getattr(mod, mod.__name__).permission} )'
