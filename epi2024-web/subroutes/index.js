425c03c6b0a87a7685a596ee71a59302495de258
ThalusA <alexandre.monier@epitech.eu>
2019-08-23 00:05:39 +0200

    Fucking asynchronous functions.

--- epi2024-web/subroutes/index.js
+++ epi2024-web/subroutes/index.js
@@ -8,27 +8,35 @@ function modulesList() {
     });
 }
 
-function createJSON(modules) {
+function createJSON() {
     let count = 0;
     let dataString = {
         "New Module": {
             data: ""
         }
     };
-    for (let i = 0; i < modules.length; i++)
-        fs.readFile("../modules/" + modules[i], (err, data) => {
-            if (err) throw err;
-            dataString[modules[idx]] = {
-                data: JSON.parse(data)
-            };
-            count++;
-            if (count == modules.length) return dataString;
+    fs.readdir('../modules', (err, files) => {
+        if (err) throw err;
+        let modules = files.filter(function (f) {
+            ext = f.substr(-3);
+            return ((ext === ".js" || ext === ".py") && f != "__pycache__");
         });
+        for (let i = 0; i < modules.length; i++)
+            fs.readFile("../modules/" + modules[i], (err, data) => {
+                if (err) throw err;
+                dataString[modules[idx]] = {
+                    data: JSON.parse(data)
+                };
+                count++;
+                if (count == modules.length) return dataString;
+            });
+    });
+    
 }
 
 module.exports = (req, res, next) => {
     const modules = modulesList();
-    const previewFiles = createJSON(modules);
+    const previewFiles = createJSON();
     /* RENDER ALL VAR */
     res.render('index', {
         title: "Epi2024 Bot - Home",
